f3f989d6c8fcb08fda860b0a6b606fddc9eaddc9
Pavel Samsonenka <pavel.samsonenka92@gmail.com>
2019-05-11 17:41:21 +0200

    add showNames

--- src/main/java/com/ak/demoGif/controller/GifController.java
+++ src/main/java/com/ak/demoGif/controller/GifController.java
@@ -1,18 +1,21 @@
 package com.ak.demoGif.controller;
 
+import com.ak.demoGif.model.repo.GifRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
 @Controller
 public class GifController {
+
+    @Autowired
+    GifRepo gifRepo;
 
     @RequestMapping("/showGifsInBrowser")
     @ResponseBody
     public String showGifsInBrowser(){
 
-
-        return "gift";
+        return gifRepo.showNames();
     }
 }
--- src/main/java/com/ak/demoGif/model/repo/GifRepo.java
+++ src/main/java/com/ak/demoGif/model/repo/GifRepo.java
@@ -1,10 +1,12 @@
 package com.ak.demoGif.model.repo;
 
 import com.ak.demoGif.model.Gif;
+import org.springframework.stereotype.Repository;
 
 import java.util.Arrays;
 import java.util.List;
 
+@Repository
 public class GifRepo {
 
     private static List<Gif> ALL_GIFS = Arrays.asList(
@@ -15,4 +17,17 @@ public class GifRepo {
             new Gif("cowboy-coder.gif", "code", true),
             new Gif("infinite-andrew.gif", "andrew", true)
     );
+
+    public String showNames(){
+
+        String name = "";
+
+        for (int i = 0; i < ALL_GIFS.size(); i++) {
+            name += ALL_GIFS.get(i).getName() + " ";
+        }
+
+        return name;
+    }
+
+
 }
